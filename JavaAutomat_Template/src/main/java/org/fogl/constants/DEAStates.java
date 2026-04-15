601e61394930caf25a27c9f4fd44285f791ee09b
Julia Prinz <2510859027@hochschule-burgenland.at>
2026-04-15 08:13:55 +0200

    States implementiert

--- JavaAutomat_Template/src/main/java/org/fogl/constants/DEAStates.java
+++ JavaAutomat_Template/src/main/java/org/fogl/constants/DEAStates.java
@@ -1,4 +1,20 @@
 package org.fogl.constants;
 
 public class DEAStates {
+
+    public static final char STATE_START = 'S';
+    public static final char STATE_END_Z = 'Z';
+    public static final char STATE_INVALID = 'X';
+
+    public  static final char STATE_A = 'A';
+    public  static final char STATE_B = 'B';
+    public  static final char STATE_C = 'C';
+    public  static final char STATE_D = 'D';
+    public  static final char STATE_E = 'E';
+    public  static final char STATE_F = 'F';
+    public  static final char STATE_G = 'G';
+    public  static final char STATE_Z = 'Z';
+
+
+
 }
