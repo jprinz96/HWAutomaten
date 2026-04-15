e796006f6c390d437c94fb7bd26627d97c70d0b9
Julia Prinz <2510859027@hochschule-burgenland.at>
2026-04-15 07:47:59 +0200

    test wg git

--- JavaAutomat_Template/src/main/java/org/fogl/Main.java
+++ JavaAutomat_Template/src/main/java/org/fogl/Main.java
@@ -3,5 +3,6 @@ package org.fogl;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Heute programmieren wir einen Automaten");
+        System.out.println("test ob das mit git jetzt geht");
     }
 }
