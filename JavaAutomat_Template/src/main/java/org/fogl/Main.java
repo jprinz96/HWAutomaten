98537bdc68a4ab9e7bbb812960b7107b378964c8
Julia Prinz <2510859027@hochschule-burgenland.at>
2026-04-15 07:52:20 +0200

    test 2

--- JavaAutomat_Template/src/main/java/org/fogl/Main.java
+++ JavaAutomat_Template/src/main/java/org/fogl/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Heute programmieren wir einen Automaten");
         System.out.println("test ob das mit git jetzt geht");
+        System.out.println("noch ein test");
     }
 }
